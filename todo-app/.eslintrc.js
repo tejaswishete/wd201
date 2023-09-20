5cfd1c417f2fafb62d0178fc268cfc7f592e302e
tejaswishete <141716600+tejaswishete@users.noreply.github.com>
2023-09-20 15:41:48 +0530

    Add files via upload

---
+++ todo-app/.eslintrc.js
@@ -0,0 +1,18 @@
+//eslintrc
+
+module.exports = {
+  
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: "eslint:recommended",
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {},
+  };
