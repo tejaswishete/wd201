6ef0777d73fb6961083d96c40b46269c83b74ea5
tejaswishete <141716600+tejaswishete@users.noreply.github.com>
2023-10-09 14:58:11 +0530

    Delete todo-app/index.js

--- todo-app/index.js
+++
@@ -1,8 +0,0 @@
-// index.js file
-const app = require("./app"); 
-const listenport = process.env.PORT || 3000; 
-  
-app.listen(listenport, () => { 
-   console.log(`Started express server at port ${listenport}`); 
- });
- 
