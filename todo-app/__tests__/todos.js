2b8de105ef49ecf7725f5ab7d640fc2f5959dca4
tejaswishete <141716600+tejaswishete@users.noreply.github.com>
2023-10-16 10:22:54 +0530

    Update todos.js

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -5,12 +5,10 @@ const db = require("../models/index");
 const app = require("../app");
 
 let server, agent;
-
 function extractCsrfToken(res) {
   const $ = cheerio.load(res.text);
   return $("[name=_csrf]").val();
 }
-
 const login = async (agent, username, password) => {
   let res = await agent.get("/login");
   let csrfToken = extractCsrfToken(res);
@@ -20,23 +18,19 @@ const login = async (agent, username, password) => {
     _csrf: csrfToken,
   });
 };
-
 describe("Todo test suite", () => {
   beforeAll(async () => {
     await db.sequelize.sync({ force: true });
     server = app.listen(3000, () => {});
     agent = request.agent(server);
   });
-
   afterAll(async () => {
     await db.sequelize.close();
     server.close();
   });
-
   test("Sign up", async () => {
     let res = await agent.get("/signup");
     const csrfToken = extractCsrfToken(res);
-
     res = await agent.post("/users").send({
       firstName: "Test",
       lastName: "User A",
@@ -46,7 +40,6 @@ describe("Todo test suite", () => {
     });
     expect(res.statusCode).toBe(302);
   });
-
   test("Sign out", async () => {
     let res = await agent.get("/todos");
     expect(res.statusCode).toBe(200);
@@ -57,7 +50,6 @@ describe("Todo test suite", () => {
     res = await agent.get("/todos");
     expect(res.statusCode).toBe(302);
   });
-
   test("Creates a todo and responds with json at /todos POST endpoint", async () => {
     const agent = request.agent(server);
     await login(agent, "user.a@test.com", "12345678");
@@ -102,31 +94,6 @@ describe("Todo test suite", () => {
     const parsedUpdateResponse = JSON.parse(response.text);
     expect(parsedUpdateResponse.completed).toBe(true);
   });
-
-  // test("Fetches all todos in the database using /todos endpoint", async () => {
-  //   const agent = request.agent(server);
-  //   await login(agent, "user.a@test.com", "12345678");
-
-  //   await agent.post("/todos").send({
-  //     title: "Buy xbox",
-  //     dueDate: new Date().toISOString(),
-  //     completed: false,
-  //   });
-  //   await agent.post("/todos").send({
-  //     title: "Buy ps3",
-  //     dueDate: new Date().toISOString(),
-  //     completed: false,
-  //   });
-  //   const response = await agent.get("/todos");
-  //   if (response.statusCode !== 200) {
-  //     throw new Error(
-  //       `Failed to fetch todos. Status code: ${response.statusCode}`,
-  //     );
-  //   }
-  //   const parsedResponse = JSON.parse(response.text);
-  //   expect(parsedResponse.length).toBe(4);
-  //   expect(parsedResponse[3]["title"]).toBe("Buy ps3");
-  // });
 
   test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
     const agent = request.agent(server);
